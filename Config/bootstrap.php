606c6d1cdea8068e749fd52146274623bb94241b
Marko Marković <okram@civokram.com>
2013-03-29 17:18:18 +0100

    Adding endpoint URL

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -8,3 +8,10 @@
 if (!Configure::read('Persona.scriptUrl')) {
 	Configure::write('Persona.scriptUrl', 'https://login.persona.org/include.js');
 }
+
+/**
+ * URL of the service that's going to verify the indentity assertion
+ */
+if (!Configure::read('Persona.endpointUrl')) {
+	Configure::write('Persona.endpointUrl', 'https://verifier.login.persona.org/verify');
+}
